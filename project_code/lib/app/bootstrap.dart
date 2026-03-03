4f403a1112c94491b0aabe0c0f5ac2fc6d4f156e
Mohamed Elsaeed <mohamed.elsaeed276@gmail.com>
2026-03-03 12:45:59 +0300

    Update Firebase initialization to use platform-specific options

--- project_code/lib/app/bootstrap.dart
+++ project_code/lib/app/bootstrap.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hatim_program/firebase_options.dart';
 
 import 'app.dart';
 
 Future<void> bootstrapApp() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const HatimProgramApp());
 }
